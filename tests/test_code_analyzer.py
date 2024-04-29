009d2bea7819aaf3e2ee445b1f5df84e44a83993
Jacek Spalinski <jfspalinski@gmail.com>
2024-04-29 17:12:50 +0200

    Add unit tests for indentation, comments and blank lines checks

--- tests/test_code_analyzer.py
+++ tests/test_code_analyzer.py
@@ -1,17 +1,77 @@
 import unittest
-from code_analyzer import analyze_code
+from code_analyzer import CodeAnalyzer
+
+
+TEST_CODE_FILE_NAME = "test_code_file.txt"
 
 
 class TestCodeAnalyzer(unittest.TestCase):
 
-    def test_line_length(self):
+    @classmethod
+    def setUp(cls):
+        cls.code_analyzer_to_test = CodeAnalyzer(TEST_CODE_FILE_NAME)
+
+    def test_check_line_length(self):
         # given
-        file_name = "long_lines.txt"
         expected_errors = ["Line 3: S001 Too long",
-                           "Line 5: S001 Too long"]
+                           "Line 6: S001 Too long"]
 
         # when
-        actual_errors = analyze_code(file_name)
+        actual_errors = self.code_analyzer_to_test.check_line_length()
+
+        # then
+        self.assertListEqual(expected_errors, actual_errors)
+
+    def test_check_indentation(self):
+        # given
+        expected_errors = ["Line 12: S002 Indentation is not a multiple of four"]
+
+        # when
+        actual_errors = self.code_analyzer_to_test.check_indentation()
+
+        # then
+        self.assertListEqual(expected_errors, actual_errors)
+
+    def test_check_comments(self):
+        # given
+        expected_errors = ["Line 1: S004 At least two spaces required before inline comments",
+                           "Line 2: S003 Unnecessary semicolon",
+                           "Line 3: S003 Unnecessary semicolon",
+                           "Line 13: S003 Unnecessary semicolon",
+                           "Line 13: S004 At least two spaces required before inline comments",
+                           "Line 13: S005 TODO found"]
+
+        # when
+        actual_errors = self.code_analyzer_to_test.check_comments()
+
+        # then
+        self.assertListEqual(expected_errors, actual_errors)
+
+    def test_blank_lines(self):
+        # given
+        expected_errors = ["Line 11: S006 More than two blank lines used before this line"]
+
+        # when
+        actual_errors = self.code_analyzer_to_test.check_blank_lines()
+
+        # then
+        self.assertListEqual(expected_errors, actual_errors)
+
+    def test_analyze_code(self):
+        # given
+        expected_errors = ["Line 1: S004 At least two spaces required before inline comments",
+                           "Line 2: S003 Unnecessary semicolon",
+                           "Line 3: S001 Too long",
+                           "Line 3: S003 Unnecessary semicolon",
+                           "Line 6: S001 Too long",
+                           "Line 11: S006 More than two blank lines used before this line",
+                           "Line 12: S002 Indentation is not a multiple of four",
+                           "Line 13: S003 Unnecessary semicolon",
+                           "Line 13: S004 At least two spaces required before inline comments",
+                           "Line 13: S005 TODO found"]
+
+        # when
+        actual_errors = self.code_analyzer_to_test.analyze_code()
 
         # then
         self.assertListEqual(expected_errors, actual_errors)
